d3c4522d951cd492404ec50e25849ab7541b799c
Unho Yeo <yuh001205@gmail.com>
2023-03-22 18:27:45 +0900

    약수 구하기 문제1 : 스스로 생각한 코드

---
+++ src/Baekjoon/Divisor.java
@@ -0,0 +1,33 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+// 2501번 약수 구하기
+public class Divisor {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+
+        int blank = line.indexOf(" ");
+        int N = Integer.parseInt(line.substring(0, blank));
+        int K = Integer.parseInt(line.substring(blank + 1));
+
+        ArrayList<Integer> divisor = new ArrayList<>();
+
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                divisor.add(i);
+            }
+        }
+
+        if (divisor.size() < K) {
+            System.out.print("0");
+        } else {
+            System.out.print(divisor.get(K - 1));
+        }
+    }
+}
